38f2de9f11838385968aad45e97675aa7436476d
jhmoon <dothebestmoon1016@gmail.com>
2022-11-28 13:43:35 +0900

    폰트 수정 및 eslint package.json 수정

--- dapp/pages/_app.tsx
+++ dapp/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "@components/common/Layout";
 import "animate.css";
+import "../public/static/fonts/style.css"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
